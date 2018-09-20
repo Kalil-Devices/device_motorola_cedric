259b7c612e1663a1796cdfcad7d1fd0863f9a278
Pedro Kalil <kalilgamer0@gmail.com>
2018-09-20 11:21:42 -0300

    cedric: use power@1.1-qti hal. * 1.0 Isnt fully set up on KAF and some AOSP roms yet

--- device.mk
+++ device.mk
@@ -303,7 +303,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti \
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
