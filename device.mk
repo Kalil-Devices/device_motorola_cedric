4cd9b560ea157e67541a82976b141713720d42ed
KalilDev <kalilgamer0@gmail.com>
2019-02-06 16:33:52 -0200

    LineageActions --> MotoActions

--- device.mk
+++ device.mk
@@ -115,9 +115,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfig
 
-# LineageActions
+# MotoActions
 PRODUCT_PACKAGES += \
-    LineageActions
+    MotoActions
 
 # Display
 PRODUCT_PACKAGES += \
