088359c17ed94722d15ace7dd0e6ae85fa838e58
KalilDev <kalilgamer0@gmail.com>
2019-02-16 13:06:26 -0200

    cedric: Properly override hidl base and manager
    * Workaround
    * Rename LOCAL_MODULE and set LOCAL_MODULE_STEM, so it replaces the original lib with the dummy one

--- device.mk
+++ device.mk
@@ -418,8 +418,8 @@ PRODUCT_COPY_FILES += \
 
 # TEMP FIX
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0_system
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
